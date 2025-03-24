a1f81c727813eff82f061708181858efce80192a
ruslanb1 <100086355+ruslanb1@users.noreply.github.com>
2025-03-24 23:00:05 +0500

    Add files via upload

---
+++ mobile-navigation-animation/dist/proj/al-html5/files/jquery/jquery-doubleTap.js
@@ -0,0 +1,39 @@
+/**
+ * jQuery doubleTap() plugin
+ * https://github.com/dzervoudakes/jQuery-doubleTap
+ * 
+ * Custom jQuery event method that triggers a callback after double-tapping/double-clicking on the selected DOM element
+ * 
+ * Copyright (c) 2015, 2016 Dan Zervoudakes
+ * Released under the MIT license
+ * https://github.com/dzervoudakes/jQuery-doubleTap/blob/master/LICENSE
+ */
+
+(function($) {
+	
+	$.fn.doubleTap = function(callback) {
+		
+		var active, interaction;
+		
+		active = false;
+		interaction = 'ontouchend' in document ? 'touchend' : 'click';
+			
+		$(this).on(interaction, function() {
+			
+			if (active) {
+				callback();
+				return active = false;
+			}
+			
+			active = true;
+			
+			setTimeout(function() {
+				active = false;
+			}, 350);
+			
+		});
+		
+		return this;
+	};
+	
+}(jQuery));
