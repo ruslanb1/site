c0e87b1bcab50941bae894ef7d6802183bf695bc
ruslanb1 <100086355+ruslanb1@users.noreply.github.com>
2025-05-03 17:42:23 +0500

    Delete mobile-navigation-animation/dist/proj/al-html5/displacement directory

--- mobile-navigation-animation/dist/proj/al-html5/displacement/buttons.js
+++
@@ -1,2 +0,0 @@
-
-$(function(){$("#stop").button({text:!1,icons:{primary:"ui-icon-arrowrefresh-1-e"}}).click(function(){playMovie=!1,dt=dtSlow,restart(),$("#play").button("option",{label:"Έναρξη",icons:{primary:"ui-icon-play"}})}),$("#allTime").button({text:!1}).click(function(){flag=$(this).is(":checked")}),$("#infoIsVisible").button({text:!1}).click(function(){valuesText.visible=$(this).is(":checked"),stage.update()}),$("#position").button({text:!1}).click(function(){xVec.visible=xVecText.visible=$(this).is(":checked"),stage.update()})});
