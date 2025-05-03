a472a893fb30646c832106b8ea5158f7fded0bae
ruslanb1 <100086355+ruslanb1@users.noreply.github.com>
2025-05-03 17:43:07 +0500

    Add files via upload

---
+++ mobile-navigation-animation/dist/proj/al-html5/displacement/buttons.js
@@ -0,0 +1,6 @@
+﻿/*!
+* © by Sitsanlis Ilias
+* This notice shall be included in all copies or substantial portions of the Software.
+* Creative Commons 4.0/CC BY - NC - SA 4.0. 
+*/
+$(function(){$("#stop").button({text:!1,icons:{primary:"ui-icon-arrowrefresh-1-e"}}).click(function(){playMovie=!1,dt=dtSlow,restart(),$("#play").button("option",{label:"Έναρξη",icons:{primary:"ui-icon-play"}})}),$("#allTime").button({text:!1}).click(function(){flag=$(this).is(":checked")}),$("#infoIsVisible").button({text:!1}).click(function(){valuesText.visible=$(this).is(":checked"),stage.update()}),$("#position").button({text:!1}).click(function(){xVec.visible=xVecText.visible=$(this).is(":checked"),stage.update()})});
